4920974d1d007a012201feda60b705adc0277c41
EthanG78 <ethangarnier78@gmail.com>
2024-11-28 10:53:49 -0400

    shear_rotation function has been fixed to removed all aliasing on angles that aren't integer multiples of 90 degrees. This has been accomplished thanks to Paeth's paper on a fast algorithm for general raster rotation.

--- code/main.m
+++ code/main.m
@@ -9,11 +9,16 @@ square = rgb2gray(square);
 shapes = rgb2gray(shapes);
 
 % figure();
-% imshow(square);
-% 
-% figure();
 % imshow(shapes);
 
-[match, matchIndicies] = template_match(shapes, square, 0.06);
+% figure();
+% imshow(square); 
+
+square_rot_45 = shear_rotation(square, 45);
+
+figure()
+imshow(square_rot_45);
+
+[match, matchIndicies] = template_match(shapes, square_rot_45, 0.02);
 
 draw_rects(shapes, size(square), matchIndicies, zeros(size(matchIndicies)));
--- code/shear_rotation.m
+++ code/shear_rotation.m
@@ -4,39 +4,51 @@ function rotated_img = shear_rotation(img, angle)
 %   image, img, around its center by angle degrees and return
 %   this rotated image
 
-    % Store the provided angle in radians
+    % Store the provided angle in radians.
     theta = deg2rad(angle);
 
-    % Create the image to be rotated
-    [M, N] = size(img); 
-    rotated_img = uint8(zeros(M, N));
+    % Create the image to be rotated. It must be a padded version
+    % of the original image to allow for the rotation to not be clipped.
+    % The minimum padding required, i.e., 2*z, where z is the hypotenuse of
+    % the width and height of the image being rotated.
+    [orig_M, orig_N] = size(img); 
+    z = ceil(sqrt((orig_M/2)^2 + (orig_N/2)^2));
+    M = 2*z;
+    N = M;
+    rotated_img = uint8(255 * ones(M, N));
 
-    % Compute the mid points by which we rotate around
-    mid_x = ceil((N + 1) / 2);
-    mid_y = ceil((M + 1) / 2);
+    % Compute the mid points by which we rotate around in original image.
+    orig_mid_x = round((orig_N + 1) / 2);
+    orig_mid_y = round((orig_M + 1) / 2);
 
-    % Define the shear matricies and the rotation transform
+    % Compute the mid points by which we rotate around in new image.
+    mid_x = round((N + 1) / 2);
+    mid_y = round((M + 1) / 2);
+
+    % Define the rotations variables used in shearing.
     a = -tan(theta/2);
     b = sin(theta);
-    shear_1 = [1 a; 0 1];
-    shear_2 = [1 0; b 1];
-    rot_mat = shear_1 * shear_2 * shear_1;
 
     % Perform the rotation
-    for i = 0:M-1
-        for j = 0:N-1
-            % Multiply the current x and y by the
-            % rotation matrix. Subtract mid point
-            % to ensure we rotate around the center.
-            xy = rot_mat * [i - mid_x ; j - mid_y];
+    for j = 0:orig_N-1
+        for i = 0:orig_M-1
+            % Define the x and y coordinates relative
+            % to the center of the image.
+            x = j - orig_mid_x;
+            y = i - orig_mid_y;
 
-            % Round the rotated coordinates to a whole
-            % number and add back the mid points.
-            x = round(xy(1)) + 1 + mid_x;
-            y = round(xy(2)) + 1 + mid_y;
-
-            if (x >= 1 && y >= 1 && x <= N && y <= M)
-                rotated_img(x, y) = img(i + 1, j + 1);
+            % Three Shear Rotation steps from Paeth.
+            x1 = round(x+y*a);        
+            y1 = round(x1*b+y);
+            x1 = round(x1+y1*a);       
+    
+            % Rotate around the center of the new image.
+            x1 = mid_x - x1;
+            y1 = mid_y - y1;
+            
+            % Update the pixel values in the new rotated image.
+            if (x1 > 0 && y1 > 0 && x1 <= N && y1 <= M)
+                rotated_img(y1, x1) = img(i + 1, j + 1);
             end
         end
     end
